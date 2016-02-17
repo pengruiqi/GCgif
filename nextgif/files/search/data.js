4b7ab745cc9eec7d09b8350c5a84bff3452671e5
pengruiqi <1299931948@qq.com>
2016-02-17 16:53:20 +0800

    giggc

---
+++ nextgif/files/search/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(r,s,t,u,v,_(w,x,y,z),A,null,B,u,C,u,D,E,F,null,G,H,I,J,K,L,M,H),N,_(),O,_(),P,_(Q,[])),R,_(),S,_());}; 
+var b="url",c="search.html",d="generationDate",e=new Date(1455698704768.74),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="a067fcf560c54b11ba2c7ee5fae9e2ef",m="type",n="Axure:Page",o="name",p="search",q="style",r="baseStyle",s="627587b6038d43cca051c114ac41ad32",t="pageAlignment",u="near",v="fill",w="fillType",x="solid",y="color",z=0xFFFFFFFF,A="image",B="imageHorizontalAlignment",C="imageVerticalAlignment",D="imageRepeat",E="auto",F="favicon",G="sketchFactor",H="0",I="colorStyle",J="appliedColor",K="fontName",L="Applied Font",M="borderWidth",N="adaptiveStyles",O="interactionMap",P="diagram",Q="objects",R="masters",S="objectPaths";
+return _creator();
+})());
